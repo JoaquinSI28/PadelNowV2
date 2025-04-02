7ab75e040a1599e057e85347616dd959a40e147e
JoaquinSI28 <97813674+JoaquinSI28@users.noreply.github.com>
2025-04-02 22:57:12 +0000

    Mensaje del cambio

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -7,11 +7,25 @@ import express, { application } from 'express';
 //import path from 'path';
 import morgan from 'morgan'
 
-const app = express();
+const database = require("./database.js");
 
-app.use(express.json())
-app.use(morgan("dev"))
+//Configuracion Inicial
+const app = express();
 
 app.set("port", 4000)
 app.listen(app.get("port"))
 console.log("Escuchando comunicaciones al puerto" + app.get("port"));
+
+
+//Middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+
+//Rutas
+
+app.get("/cancha", async(req,res) =>{
+    const connection = await database.getConnection();
+    const result = await connection.query("SELECT * from cancha");
+    console.log(result)
+})
+
