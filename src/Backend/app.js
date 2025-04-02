bf1db8d026bc32dea823d5b9b21dab734c6559e4
JoaquinSI28 <97813674+JoaquinSI28@users.noreply.github.com>
2025-04-02 18:53:53 +0000

    Mensaje del cambio

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -5,8 +5,12 @@ import express, { application } from 'express';
 //import jwt from 'jsonwebtoken';
 //import cors from 'cors';
 //import path from 'path';
+import morgan from 'morgan'
 
 const app = express();
+
+app.use(express.json())
+app.use(morgan("dev"))
 
 app.set("port", 4000)
 app.listen(app.get("port"))
