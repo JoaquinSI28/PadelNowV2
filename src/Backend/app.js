771d9e526468f7c98bcb6c465d76f68cea4ed932
JoaquinSI28 <97813674+JoaquinSI28@users.noreply.github.com>
2025-04-03 01:52:46 +0000

    Mensaje del cambio

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -12,7 +12,7 @@ import database from "./database.js";
 //Configuracion Inicial
 const app = express();
 
-app.set("port", 3306)
+app.set("port", 4000)
 app.listen(app.get("port"))
 console.log("Escuchando comunicaciones al puerto" + app.get("port"));
 
